d3d4426238f3c5da4fc30334e183b664049e3874
Arthur Carson <rthrcrsn@gmail.com>
2020-12-13 19:09:39 -0600

    db and py connected

--- app.py
+++ app.py
@@ -5,94 +5,96 @@ import os
 
 # init app
 app = Flask(__name__)
-basedir = os.path.abspath(os.path.dirname(__file__))
+# basedir = os.path.abspath(os.path.dirname(__file__))
 
 # Database
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'db.sqlite')
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://root:Woxnsk19@localhost/cast_watch'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 # init db
 db = SQLAlchemy(app)
 # init marshmallow
-ma = Marshmallow(app)
+mv = Marshmallow(app)
 
 
-# Product class
-class Product(db.Model):
+# Movie class
+class Movie(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(100), unique=True)
-    description = db.Column(db.String(200))
-    price = db.Column(db.Float)
-    qty = db.Column(db.Integer)
+    title = db.Column(db.String(300), unique=True)
+    overview = db.Column(db.String(2000))
+    release_date = db.Column(db.String(12))
+    poster_path = db.Column(db.String(60))
 
-    def __init__(self, name, description, price, qty):
-        self.name = name
-        self.description = description
-        self.price = price
-        self.qty = qty
+    def __init__(self, title, overview, release_date, poster_path):
+        self.title = title
+        self.overview = overview
+        self.release_date = release_date
+        self.poster_path = poster_path
 
 
-# Product Schema
-class ProductSchema(ma.Schema):
+
+
+# Movie Schema
+class MovieSchema(mv.Schema):
     class Meta:
-        fields = ('id', 'name', 'description', 'price', 'qty')
+        fields = ('id', 'title', 'overview', 'release_date', 'poster_path')
 
 
 # Init schema
-product_schema = ProductSchema()
-products_schema = ProductSchema(many=True)
+movie_schema = MovieSchema()
+movies_schema = MovieSchema(many=True)
 
 
-# Create a Product
-@app.route('/product', methods=['POST'])
-def add_product():
-    name = request.json['name']
-    description = request.json['description']
-    price = request.json['price']
-    qty = request.json['qty']
-    new_product = Product(name, description, price, qty)
-    db.session.add(new_product)
+# Create a Movie
+@app.route('/movies', methods=['POST'])
+def add_movie():
+    title = request.json['title']
+    overview = request.json['overview']
+    release_date = request.json['release_date']
+    poster_path = request.json['poster_path']
+    new_movie = Movie(title, overview, release_date, poster_path)
+    db.session.add(new_movie)
     db.session.commit()
-    return product_schema.jsonify(new_product)
+    return movie_schema.jsonify(new_movie)
 
 
-# Get all products
-@app.route('/product', methods=['GET'])
-def get_products():
-    all_products = Product.query.all()
-    result = products_schema.dump(all_products)
+# Get all Movies
+@app.route('/movies', methods=['GET'])
+def get_movies():
+    all_movies = Movie.query.all()
+    result = movies_schema.dump(all_movies)
     return jsonify(result)
 
 
-# Get one product
-@app.route('/product/<id>', methods=['GET'])
-def get_product(id):
-    product = Product.query.get(id)
-    return product_schema.jsonify(product)
+# # Get one product
+# @app.route('/product/<id>', methods=['GET'])
+# def get_product(id):
+#     product = Product.query.get(id)
+#     return product_schema.jsonify(product)
 
 
-# Update a Product
-@app.route('/product/<id>', methods=['PUT'])
-def update_product(id):
-    product = Product.query.get(id)
-    name = request.json['name']
-    description = request.json['description']
-    price = request.json['price']
-    qty = request.json['qty']
-    product.name = name
-    product.description = description
-    product.price = price
-    product.qty = qty
-    db.session.commit()
-    return product_schema.jsonify(product)
+# # Update a Product
+# @app.route('/product/<id>', methods=['PUT'])
+# def update_product(id):
+#     product = Product.query.get(id)
+#     name = request.json['name']
+#     description = request.json['description']
+#     price = request.json['price']
+#     qty = request.json['qty']
+#     product.name = name
+#     product.description = description
+#     product.price = price
+#     product.qty = qty
+#     db.session.commit()
+#     return product_schema.jsonify(product)
 
 
-# Delete Products
-@app.route('/product/<id>', methods=['DELETE'])
-def delete_product(id):
-    product = Product.query.get(id)
-    db.session.delete(product)
-    db.session.commit()
-    return product_schema.jsonify(product)
+# # Delete Products
+# @app.route('/product/<id>', methods=['DELETE'])
+# def delete_product(id):
+#     product = Product.query.get(id)
+#     db.session.delete(product)
+#     db.session.commit()
+#     return product_schema.jsonify(product)
 
 
 # run server
